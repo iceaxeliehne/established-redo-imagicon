ba43336e252607c0343e1bb3dfdd8ebb22759c2f
iceaxeliehne <iceaxe.liehne@gmail.com>
2021-02-11 04:23:19 +0000

    socket connection working

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
-const message = "Hello World";
+const express = require('express')
+var app = express();
+var server = app.listen(3000);
+var io = require('socket.io')(server);
 
-console.log(message, "from Gitpod!");
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+})
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+  socket.on('stream', (data) => {
+    console.log('message: ' + data);
+    socket.broadcast.emit('player',data);
+  })
+});
+
+//app.listen(3000, () => console.log('Server ready'))

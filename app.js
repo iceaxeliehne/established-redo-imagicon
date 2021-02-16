379a0b76b3255dbf0806bb102305b4df7f74567e
iceaxeliehne <iceaxe.liehne@gmail.com>
2021-02-16 05:01:44 +0000

    streaming connection working

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express')
 var app = express();
 var server = app.listen(3000);
 var io = require('socket.io')(server);
+const sharp = require('sharp');
+
+var frameCount = 0;
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
@@ -12,9 +15,12 @@ io.on('connection', (socket) => {
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
-  socket.on('stream', (data) => {
-    console.log('message: ' + data);
-    socket.broadcast.emit('player',data);
+  socket.on('myStream', (data) => {
+      frameCount++;
+    console.log('frames recieved ', frameCount);
+    console.log(typeof(data));
+    socket.emit('player',data);
+    //socket.emit('player',sharp(data).rotate());
   })
 });
 
